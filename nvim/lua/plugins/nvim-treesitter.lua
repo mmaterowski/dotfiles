498975e53406b3d791a49655c3d7582035ebc320
Michal Materowski <michal.materowski@gmail.com>
2025-11-21 10:49:24 +0100

    add svelte

--- nvim/lua/plugins/nvim-treesitter.lua
+++ nvim/lua/plugins/nvim-treesitter.lua
@@ -2,8 +2,12 @@ return {
   "nvim-treesitter",
   opts = function(_, opts)
     if type(opts.ensure_installed) == "table" then
-      vim.list_extend(opts.ensure_installed, { "angular", "scss" })
+      vim.list_extend(opts.ensure_installed, { "svelte", "typescript", "javascript", "angular", "scss" })
     end
+
+    opts.highlight = opts.highlight or {}
+    opts.highlight.enable = true
+
     vim.api.nvim_create_autocmd({ "BufReadPost", "BufNewFile" }, {
       pattern = { "*.component.html", "*.container.html" },
       callback = function()
